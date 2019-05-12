88cf1129625e9ac3d1f2c92af8636e334c27d01c
Cristian Moraru <cristian.moraru@live.com>
2019-05-12 19:27:55 +0200

    Finished implementing HttpContextExecutor.

--- src/executor/implementations/HttpContextExecutor.php
+++ src/executor/implementations/HttpContextExecutor.php
@@ -2,8 +2,23 @@
 
 namespace Comertis\Http\Internal;
 
+use Comertis\Http\HttpResult;
+use Comertis\Http\HttpRequest;
+use Comertis\Http\HttpResponse;
+use Comertis\Http\HttpRequestMethod;
+use Comertis\Http\HttpRequestBodyType;
+use Comertis\Http\Internal\IHttpExecutor;
+
 class HttpContextExecutor implements IHttpExecutor
 {
+    /**
+     * Stream context options
+     *
+     * @access private
+     * @var array
+     */
+    private $_options;
+
     /**
      * @see IHttpExecutor::prepareUrl()
      *
@@ -42,7 +57,20 @@ class HttpContextExecutor implements IHttpExecutor
      */
     public function prepareHeaders(HttpRequest &$request)
     {
+        $this->_options = [
+            'http' => [
+                'method'  => $request->getMethod(),
+                'header' => ''
+            ]
+        ];
 
+        foreach ($request->getHeaders() as $key => $value) {
+            $this->_options['http']['header'] .= $key . "=" . $value . ";";
+        }
+
+        if (!empty($bodyType = $request->getBodyType())) {
+            $this->_options['http']['header'] .= "Content-Type: " . $bodyType . ";";
+        }
     }
 
     /**
@@ -54,7 +82,23 @@ class HttpContextExecutor implements IHttpExecutor
      */
     public function prepareParams(HttpRequest &$request)
     {
+        $params = [];
+        if (empty($params = $request->getParams())) {
+            return;
+        }
 
+        switch ($request->getBodyType()) {
+            case HttpRequestBodyType::JSON:
+                $params = json_encode($params);
+                break;
+
+            case HttpRequestBodyType::X_WWW_FORM_URLENCODED:
+            default:
+                $params = http_build_query($params);
+                break;
+        }
+
+        $this->_options['http']['content'] = $params;
     }
 
     /**
@@ -66,6 +110,48 @@ class HttpContextExecutor implements IHttpExecutor
      */
     public function execute(HttpRequest $request)
     {
-        
+        $responseInfo = [];
+        $responseHeaders = [];
+        $responseBody = null;
+        $responseStatusCode = 500;
+
+        $context = stream_context_create($this->_options);
+
+        $stream = @fopen($request->getUrl(), 'r', false, $context);
+
+        if ($stream) {
+            $responseInfo = stream_get_meta_data($stream);
+        } else {
+            $responseInfo['wrapper_data'] = $http_response_header;
+        }
+
+        // Headers
+        $headers = $responseInfo['wrapper_data'];
+        for ($i = 1; $i < count($headers); $i++) {
+            $currentHeader = explode(":", $headers[$i], 2);
+            $responseHeaders[\trim($currentHeader[0])] = \trim($currentHeader[1]);
+        }
+
+        // Body
+        if ($stream) {
+            $responseBody = stream_get_contents($stream);
+        }
+
+        // Status Code
+        $match = [];
+        $status_line = $responseInfo['wrapper_data'][0];
+
+        preg_match('{HTTP\/\S*\s(\d{3})}', $status_line, $match);
+
+        $responseStatusCode = (int)$match[1];
+
+        // Cleanup
+        if ($stream) {
+            fclose($stream);
+        }
+
+        $response = new HttpResponse($responseHeaders, $responseStatusCode, $responseBody);
+
+        return new HttpResult($request, $response);
     }
 }
