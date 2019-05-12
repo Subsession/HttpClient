dbb96f3a1888f7398681e3d8a290750e97ea8454
Cristian Moraru <cristian.moraru@live.com>
2019-05-12 19:28:57 +0200

    Fixed a HttpRequestType value.

--- src/enums/HttpRequestType.php
+++ src/enums/HttpRequestType.php
@@ -6,7 +6,7 @@ class HttpRequestBodyType
 {
     const NONE = "NONE";
     const FORM_DATA = "FORM_DATA";
-    const X_WWW_FORM_URLENCODED = "application/x_www_form_urlencoded";
+    const X_WWW_FORM_URLENCODED = "application/x-www-form-urlencoded";
     const TEXT = "application/text";
     const TEXT_PLAIN = "text/plain";
     const JSON = "application/json";
