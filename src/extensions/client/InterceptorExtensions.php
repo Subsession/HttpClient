c72cfd8efe91fac7adffc864590522165c3dab93
Cristian Moraru <cristian@comertis.com>
2019-08-23 16:22:03 +0200

    Updated license.

--- src/extensions/client/InterceptorExtensions.php
+++ src/extensions/client/InterceptorExtensions.php
@@ -3,26 +3,9 @@
  * PHP Version 7
  *
  * LICENSE:
- * Permission is hereby granted, free of charge, to any
- * person obtaining a copy of this software and associated
- * documentation files (the "Software"), to deal in the
- * Software without restriction, including without limitation
- * the rights to use, copy, modify, merge, publish, distribute,
- * sublicense, and/or sell copies of the Software, and to
- * permit persons to whom the Software is furnished to do so,
- * subject to the following conditions:
+ * Proprietary, see the LICENSE file that was provided with the software.
  *
- * The above copyright notice and this permission notice shall
- * be included in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
- * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
- * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
- * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
- * DEALINGS IN THE SOFTWARE.
+ * Copyright (c) 2019 - present Comertis <info@comertis.com>
  *
  * @category Http
  * @package  Comertis\Http
@@ -47,7 +30,7 @@ use Comertis\Http\Interceptors\HttpInterceptor;
  * @version  Release: 1.0.0
  * @link     https://github.com/Comertis/HttpClient
  */
-trait InterceptorExtension
+trait InterceptorExtensions
 {
     /**
      * HttpRequest & HttpResponse interceptor
--- src/interceptors/HttpInterceptor.php
+++ src/interceptors/HttpInterceptor.php
@@ -3,26 +3,9 @@
  * PHP Version 7
  *
  * LICENSE:
- * Permission is hereby granted, free of charge, to any
- * person obtaining a copy of this software and associated
- * documentation files (the "Software"), to deal in the
- * Software without restriction, including without limitation
- * the rights to use, copy, modify, merge, publish, distribute,
- * sublicense, and/or sell copies of the Software, and to
- * permit persons to whom the Software is furnished to do so,
- * subject to the following conditions:
+ * Proprietary, see the LICENSE file that was provided with the software.
  *
- * The above copyright notice and this permission notice shall
- * be included in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
- * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
- * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
- * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
- * DEALINGS IN THE SOFTWARE.
+ * Copyright (c) 2019 - present Comertis <info@comertis.com>
  *
  * @category Http
  * @package  Comertis\Http
@@ -65,21 +48,6 @@ class HttpInterceptor
      */
     private $response;
 
-    public function getRequest()
-    {
-        return $this->request;
-    }
-
-    public function getResponse()
-    {
-        return $this->response;
-    }
-
-    public static function build()
-    {
-        return new self();
-    }
-
     /**
      * Constructor
      */
@@ -87,5 +55,27 @@ class HttpInterceptor
     {
         $this->request = new HttpRequestInterceptor();
         $this->response = new HttpResponseInterceptor();
+    }
+
+    /**
+     * Get the request interceptor
+     *
+     * @access public
+     * @return HttpRequestInterceptor
+     */
+    public function getRequest()
+    {
+        return $this->request;
+    }
+
+    /**
+     * Get the response interceptor
+     *
+     * @access public
+     * @return HttpResponseInterceptor
+     */
+    public function getResponse()
+    {
+        return $this->response;
     }
 }
